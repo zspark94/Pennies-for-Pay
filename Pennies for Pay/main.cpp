6bbfc67ad3e8639e860ec9f0f937d8e5f487dc69
Sparky(Reakt!on) <Reaktion@Mac-Owner-3.local>
2013-10-24 10:30:28 -0500

    Initial Commit

---
+++ Pennies for Pay/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Pennies for Pay
+//
+//  Created by Sparky(Reakt!on) on 10/24/13.
+//  Copyright (c) 2013 Sparky(Reakt!on). All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
